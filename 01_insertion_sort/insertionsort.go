e18ab1fb38e05d8a6ac4d90b4bf55706334a04f6
Valentin Pecatte <valentin.pecatte@gmail.com>
2022-08-28 15:20:46 +0200

    feat(insertionsort): can change the sort order
    Adding the possibility to give a comparator, so now it is possible to give a sort order.

--- 01_insertion_sort/insertionsort.go
+++ 01_insertion_sort/insertionsort.go
@@ -1,12 +1,12 @@
 package insertionsort
 
-func InsertionSort(arr []int) []int {
+func InsertionSort(arr []int, compare func(a, b int) int) []int {
 	for i := 1; i < len(arr); i += 1 {
 		v := arr[i]
 		j := i - 1
 		// shift all sorted elements to the right until we found
 		// the place to insert v
-		for ; j > -1 && arr[j] > v; j -= 1 {
+		for ; j > -1 && compare(arr[j], v) > 0; j -= 1 {
 			arr[j+1] = arr[j]
 		}
 		arr[j+1] = v
--- 01_insertion_sort/insertionsort_test.go
+++ 01_insertion_sort/insertionsort_test.go
@@ -6,35 +6,56 @@ import (
 )
 
 func TestInsertionSort(t *testing.T) {
+	asccmp := func(a, b int) int {
+		if a > b {
+			return 1
+		} else if a == b {
+			return 0
+		}
+		return -1
+	}
+	dsccmp := func(a, b int) int {
+		if a > b {
+			return -1
+		} else if a == b {
+			return 0
+		}
+		return 1
+	}
 	cases := []struct {
 		init, exp []int
+		cmp       func(a, b int) int
 	}{
 		{
 			init: []int{1, 2},
 			exp:  []int{1, 2},
+			cmp:  asccmp,
 		},
 		{
 			init: []int{2, 1},
 			exp:  []int{1, 2},
+			cmp:  asccmp,
 		},
 		{
 			init: []int{2, 1, 3},
 			exp:  []int{1, 2, 3},
+			cmp:  asccmp,
 		},
 		{
 			init: []int{2, 1, 3, -1, -2, 10, 7},
 			exp:  []int{-2, -1, 1, 2, 3, 7, 10},
+			cmp:  asccmp,
+		},
+		{
+			init: []int{2, 1, 3, -1, -2, 10, 7},
+			exp:  []int{10, 7, 3, 2, 1, -1, -2},
+			cmp:  dsccmp,
 		},
 	}
 	for _, c := range cases {
-		got := InsertionSort(c.init)
+		got := InsertionSort(c.init, c.cmp)
 		if !reflect.DeepEqual(got, c.exp) {
 			t.Errorf("init: %v, got array %v, expecting %v", c.init, got, c.exp)
 		}
 	}
-	//arr := []int{2, 3, 1, 4, -1}
-	//exp := []int{-1, 1, 2, 3, 4}
-	//got := InsertionSort(arr)
-	//if !reflect.DeepEqual(got, exp) {
-	//}
 }
