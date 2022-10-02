ee55adc6386a1f61f4fc3439b8a5aa89cae70317
forgeronvirtuel <forgeronvirtuel@gmail.com>
2022-10-02 22:22:47 +0200

    feat: golang copy func

--- 04_copy_arrays/copy_arrays.go
+++ 04_copy_arrays/copy_arrays.go
@@ -1,10 +1,13 @@
 package _4_copy_arrays
 
-func ConcatWithCopy(s1, s2 []int) []int {
-	size := len(s1) + len(s2)
-	s := make([]int, size)
-	copy(s, s1)
-	copy(s[len(s1):], s2)
+func ConcatWithAppend(s1, s2 []int) []int {
+	var s []int
+	for i := 0; i < len(s1); i++ {
+		s = append(s, s1[i])
+	}
+	for i := 0; i < len(s2); i++ {
+		s = append(s, s2[i])
+	}
 	return s
 }
 
@@ -21,13 +24,10 @@ func ConcatWithFor(s1, s2 []int) []int {
 	return s
 }
 
-func ConcatWithAppend(s1, s2 []int) []int {
-	var s []int
-	for i := 0; i < len(s1); i++ {
-		s = append(s, s1[i])
-	}
-	for i := 0; i < len(s2); i++ {
-		s = append(s, s2[i])
-	}
+func ConcatWithCopy(s1, s2 []int) []int {
+	size := len(s1) + len(s2)
+	s := make([]int, size)
+	copy(s, s1)
+	copy(s[len(s1):], s2)
 	return s
 }
